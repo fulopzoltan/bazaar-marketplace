935de838cf0efa63380f04389d31087e5041c921
Fulop Zoltan <fulopzoltan20@gmail.com>
2021-12-20 22:23:18 +0200

    Added glider and fixed fare_item design

--- app/src/main/java/com/example/bazaar_marketplace/adapters/FareItemAdapter.kt
+++ app/src/main/java/com/example/bazaar_marketplace/adapters/FareItemAdapter.kt
@@ -3,8 +3,10 @@ package com.example.bazaar_marketplace.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.bazaar_marketplace.R
 import com.example.bazaar_marketplace.models.Product
 
@@ -14,6 +16,8 @@ class FareItemAdapter(private var fareItemList: List<Product>) :
         val itemPrice: TextView = itemView.findViewById(R.id.fare_item_price)
         val sellerName: TextView = itemView.findViewById(R.id.fare_item_seller_name)
         val itemName: TextView = itemView.findViewById(R.id.fare_item_name)
+        val fareItemImage: ImageView = itemView.findViewById(R.id.fare_item_image)
+
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FareItemViewHolder {
@@ -24,6 +28,16 @@ class FareItemAdapter(private var fareItemList: List<Product>) :
 
     override fun onBindViewHolder(holder: FareItemViewHolder, position: Int) {
         val current = fareItemList[position]
+
+        Glide.with(holder.itemView.context)
+            .load(R.drawable.fare_item_image_placeholder)
+            .placeholder(R.drawable.fare_item_image_placeholder)
+            .error(R.drawable.fare_item_image_placeholder)
+            .fitCenter()
+            .centerInside()
+            .circleCrop()
+            .into(holder.fareItemImage)
+
         holder.itemName.text = current.title
         holder.itemPrice.text =
             "${current.pricePerUnit} ${current.priceType}/ ${current.amountType}"
